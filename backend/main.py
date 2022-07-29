b9c053a8aae36b17b617de9808850a15caa664fe
zh601095001 <601095001@qq.com>
2022-07-29 13:27:31 +0800

    新建

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
+import uvicorn
 from fastapi import FastAPI, Depends, status
 from db.database import SessionLocal
 from sqlalchemy.orm import Session
 from db import crud, schemas
+from fastapi.staticfiles import StaticFiles
 
 
 def get_db():
@@ -23,8 +25,13 @@ app = FastAPI(
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
 )
+app.mount("/", StaticFiles(directory="../build", html=True), name="build")  # 挂载react编译后文件
 
 
 @app.get("/test", response_model=schemas.TestSchema)
 def Test(id: int, db: Session = Depends(get_db)):
     return crud.getById(db, id)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
