3b60baad8b0d43d4093f4319aa43f3e8c328c7a3
zh601095001 <601095001@qq.com>
2022-07-29 15:59:53 +0800

    页面布局

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Demo from "../components/Demo";
 import Demo2 from "../components/Demo2";
+import {Layout} from "antd";
+import styles from "../index.module.less"
 
+const {Header, Sider, Content} = Layout
 
 function Home() {
     return (
-        <>
-        </>
+        <Layout className={styles.layoutWhite}>
+            <Header className="header"></Header>
+            <Layout className="layout">
+                <Sider width={300} className="sider-left"></Sider>
+                <Content className="content"></Content>
+                <Sider width={300} className="sider-right"></Sider>
+            </Layout>
+        </Layout>
     );
 }
 
