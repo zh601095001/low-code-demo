ccff3d4da2d25d96c28f25286078e6d43518835f
zh601095001 <601095001@qq.com>
2022-07-29 15:20:29 +0800

    修改代理

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Demo from "../components/Demo";
 import Demo2 from "../components/Demo2";
 
+
 function Home() {
     return (
-        <div>
-            <Demo/>
-            <Demo2/>
-        </div>
+        <>
+        </>
     );
 }
 
